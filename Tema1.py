c190d0210fa5a6d1f3f52bb0275ad330366aa51d
Alin <alin.ciuta95@gmail.com>
2020-10-14 22:29:40 +0300

    Tema 1 Updated

--- Tema1.py
+++ Tema1.py
@@ -1,49 +1,78 @@
+import datetime
+import math
+
 iterator = 0
-Cuvant = input('Scrie un cuvant:\t')
-print('Cuvantul tau este {}\n'.format(Cuvant))
-for c in Cuvant:
-    print('cuvantul', Cuvant, 'are', len(Cuvant), 'caractere\n')
-    break
+caractere = 0
+count = ""
+total = None
+cuvant = str(input('Sir de caractere:\t'))
+for i in cuvant.replace(' ', ''):
+    caractere += 1
+    count += i
+print('\t', caractere, "caractere sunt in acest sir\n")
 
 email = str(input('Introdu adresa ta de email:\t'))
 print('Adresa introdusa este:\t{}'.format(email))
-if ('@' and '.') in email:
+if ('@' and '.') in email and email.count('@') == 1 and email.count('.') == 1:
     print('Adresa este VALIDA!')
 else:
     print('Adresa GRESITA!')
 
-telefon = input('Introdu numarul de telefon:\t')
-
-if len(telefon) == 10:
+telefon = str(input('Introdu numarul de telefon:\t'))
+tada = [int(i) for i in str(telefon)]
+if telefon.isdigit() == False and len(telefon) == 10 and tada[0] == 0:
     print('Numarul este valid')
 else:
     print('Numarul introdus nu exista')
 
-CNP = str(input('Introduceti Codul Numeric Personal:\t'))
+numar_control = None
+cnp1 = str(input('Introduceti Codul Numeric Personal:\t'))
+cnp = [int(i) for i in str(cnp1)]
+cod = [int(i) for i in str(cnp1)]
+gresit = 'CNP-ul introdus este GRESIT'
 control = [2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9]
-print('CNP-ul {}'.format(CNP), 'a fost introdus\n')
-if len(CNP) == 13 and ('1' or '2') in CNP[0] and ('0' or '1') in CNP[3] and ('0' or '1' or '2'or '3') in CNP[5]:
-    print('CNP-ul este in curs de validare...')
+an = None
+an1 = cnp1[1:3]
+if int(an1) in range(25):
+    an = '20'+an1
 else:
-    print('CNP gresit!')
-cod = [int(i) for i in str(CNP)]
-cod.pop()
-final_cod = [cod[i] * control[i] for i in range(len(cod))]
-
-adunare = sum(final_cod)
-rezultat = adunare / 11
-print("Se calculeaza cifra de control...")
-rezultat = round(rezultat, 1)
-rest = str(rezultat)
-cifra_control = rest[3:]
-print('Cifra de control este: ', cifra_control)
-raspunsul = None
-if rezultat == 10:
-    raspunsul = 1
+    an = '19'+an1
+luna = cnp1[3:5]
+zi = cnp1[5:7]
+data = an+'-'+luna+'-'+zi
+print(' Data nasterii tale este:\t', data)
+format = '%Y-%m-%d'
+try:
+    datetime.datetime.strptime(data, format)
+    print('Data nasteriitale este corecta')
+except ValueError:
+    print('Data nasterii tale este GRESITA')
+if cnp1.isdigit() == False or len(cnp) != 13 or int(cnp[0]) < 1 or int(cnp[0]) > 9:
+    print(gresit)
+elif int(cnp[7]) not in range(0, 5):
+    print(gresit)
+elif int(cnp[7]) == 4 and int(cnp[8]) not in range(0, 6):
+    print(gresit)
+elif int(cnp[7]) == 5 and int(cnp[8]) not in range(1, 2):
+    print(gresit)
 else:
-    raspunsul = cifra_control
-
-if raspunsul == CNP[12]:
-    print('Felicitari! CNP-ul este corect!')
+    cod.pop()
+    final_cod = [cod[i] * control[i] for i in range(len(cod))]
+    adunare = sum(final_cod)
+    rezultat = adunare / 11
+    #rezultat = round(rezultat, 2)
+    rest = str(rezultat)
+    numar_control = rest[1:]
+    k = float(numar_control)
+    def round_up(n, decimals=0):
+        multiplier = 10 ** decimals
+        return math.ceil(n * multiplier) / multiplier
+    numar_control2 = round_up(k, 1)
+    numar_control_3_3 = str(numar_control2)
+    cifra_control_ok = numar_control_3_3[2:]
+    print('Cifra de control este: ', cifra_control_ok)
+    raspunsul = None
+if cifra_control_ok == cnp1[12]:
+    print('CNP-ul este VALID')
 else:
     print('CNP-ul NU este valid!')
---
+++ for_14102020.py
@@ -0,0 +1,71 @@
+# lista = [1, 2, 3, 4]
+# for index, value in enumerate(lista):
+#     print(index, '=>', value)
+#
+# x = 'mere'
+# # x[0] = 'p'
+# y = x.replace('m', 'p')
+# print(y)
+#
+# a = 'Ana are mere'
+# print(a.split(' '))
+# #
+# a = ('a', False, 4, ('tuple', 3), 'soare')
+#
+# list(a)
+# print(list(a))
+# # b = ('a',)  #Ca un element sa fie Tuplu trebuie sa aiba virgula
+# # # print(type(a))
+# # # print(type(b))
+#
+# y = ('b', 4)
+# print(a+y)  #Putem concatena stringuri si tupluri
+# for i in y:
+#     print(i)
+# # y[0] = 'a'
+
+#  LISTA
+# x = ['a', False, 4, ('tuple', 3), 'soare']
+# for i in x:
+#     print(i)
+#
+# x[0] = 'b'
+# print(x)
+# y = [['123', False, 'abc', 'e'], [1], 'a', True, ['7', 'cdef']]
+# print(y[0][2][1]) #Acceseaza B-ul din abc
+# print(y[4][1][3])
+# print(str(y[0][0][2])) #nr 3 din primul cuv
+#
+# y.append(2)
+# y.insert(2, 'alo')
+# print(y)
+# print(y[::2]) # Afiseasa elementele din 2 in 2
+
+#  SET
+# s = {1, 2,7, 'a', 3, 4, 5}  # Setul are doar elemente unice
+# s.add(6)
+# print(s)
+# for x in set(s):
+#     print(x)
+# a = [1, 2, 3]
+# b = [4, 5, 2]
+# print(set(a) | set(b))
+# print(set(a) - set(b))
+# (s.discard(3))
+# print(s)
+
+# DICTIONAR
+# a = {1: 'abv',
+#      '2': [1, 2, 3],
+#      (1, 2): 'a'}
+# for x in a.keys():
+#     print(x)
+# a[2] = 'fff'
+# print(a)
+
+taskuri = ['citit', 'scris', 'vorbit']
+a = None
+while a != '0':
+    a = input(' Introdu task: ')
+    taskuri.append(a)
+    print(taskuri)
